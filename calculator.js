df4c6abf1eb14744f6481c811676eb7edd34dd0f
Ahmed Maher Ali Al-Maqtari <mr.numberone312@gmail.com>
2023-08-26 22:34:39 +0300

    adding JavaScript - Functionality

---
+++ calculator.js
@@ -0,0 +1,50 @@
+// globes
+let lastvalue = "";
+let operations ="*+-/";
+
+function appendToInput(value) {
+    var inputField = document.getElementById("inputField");
+    // No double operator
+    if(operations.includes(lastvalue))
+    {
+        if(operations.includes(value)){
+            var resultField = document.getElementById("resultField");
+            resultField.value = "incorrect input"
+            return;
+        }
+    }
+    lastvalue = value;
+    inputField.value += value;
+    
+  }
+  
+  function calculate() {
+    // To eleminate bad expressions like {12+5-} ending in an operator
+    if (operations.includes(lastvalue) && lastvalue != "") {
+        var resultField = document.getElementById("resultField");
+        resultField.value = "invalid expression"
+        return;
+    }else{
+        if (lastvalue == "") {
+            var resultField = document.getElementById("resultField");
+            resultField.value = "Nothing to calculate"
+            return;
+        }
+    }
+    
+    var inputField = document.getElementById("inputField");
+    var resultField = document.getElementById("resultField");
+    
+    var inputExpression = inputField.value;
+    var result = eval(inputExpression);
+
+    resultField.value = result;
+  }
+  
+  function clearCalculator() {
+    var inputField = document.getElementById("inputField");
+    var resultField = document.getElementById("resultField");
+    
+    inputField.value = "";
+    resultField.value = "";
+  }
